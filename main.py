38f5b3da8450d37d0dd2062a4f1f92626818ad35
Anurag Negi <neginaurag99@gmail.com>
2025-06-16 11:27:57 +0530

    just checking if its working or not

--- main.py
+++ main.py
@@ -6,13 +6,13 @@ load_dotenv()
 api_key = os.environ.get("GEMINI_API_KEY")
 client = genai.Client(api_key=api_key)
 
-search_string = input("What u wana Know ?\n")
+# search_string = input("What u wana Know ?\n")
 
 resp = client.models.generate_content(
-    model="gemini-2.0-flash-001", contents=search_string
+    model="gemini-2.0-flash-001", contents="Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum."
 )
 
 if resp is not None and resp.usage_metadata is not None:
-    print(f"Ans: {resp.text} ")
-    print(f"prompt_token_count: {resp.usage_metadata.total_token_count}")
-    print(f"candidate_token_count: {resp.usage_metadata.candidates_token_count}")
+    print(f"{resp.text}")
+    print(f"Prompt tokens: {resp.usage_metadata.prompt_token_count}")
+    print(f"Response tokens: {resp.usage_metadata.candidates_token_count}")
