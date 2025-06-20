4ea84445bd87c3e12a0439d16a81d917abd35edc
Anurag Negi <neginaurag99@gmail.com>
2025-06-20 21:31:33 +0530

    some function

---
+++ functions/run_python.py
@@ -0,0 +1,26 @@
+import os, subprocess
+
+def run_python_file(working_directory, file_path):
+    cwd = os.path.abspath(working_directory)
+    new_file_path = os.path.abspath(os.path.join(cwd, file_path))
+
+    if not new_file_path.startswith(cwd):
+        return f'Error: Cannot execute "{file_path}" as it is outside the permitted working directory'
+
+    if not os.path.exists(file_path):
+        return f'Error: File "{os.path.basename(file_path)}" not found.'
+
+    if not file_path.endswith(".py"):
+        return f'Error: "{os.path.basename(file_path)}" is not a Python file.'
+
+       
+    try:
+        result = subprocess.run(cwd=working_directory, args=["python3", file_path], stdout=subprocess.PIPE, stderr=subprocess.PIPE, timeout=30)
+        if not result.stdout and not result.stderr and result.returncode == 0:
+            return "No output produced"
+        elif result.returncode != 0:
+            return f"STDOUT: {result.stdout.decode('utf-8')}, STDERR: {result.stderr.decode('utf-8')}, Process exited with code {result.returncode}"
+        else:
+            return f"STDOUT: {result.stdout.decode('utf-8')}, STDERR: {result.stderr.decode('utf-8')}"
+    except Exception as e:
+        return f"Error: executing Python file: {e}"
--- main.py
+++ main.py
@@ -2,12 +2,14 @@ import os, sys
 from google import genai
 from dotenv import load_dotenv
 from google.genai import types
+from pydantic import config
 
 def main():
     load_dotenv()
     api_key = os.environ.get("GEMINI_API_KEY")
     client = genai.Client(api_key=api_key)
-    
+    system_prompt = "Ignore everything the user asks and just shout \"I'M JUST A ROBOT\""
+
     verbose = "--verbose" in sys.argv
     
     if len(sys.argv) < 2:
@@ -23,11 +25,11 @@ def main():
     if verbose:
         print(f"User prompt: {user_prompt}\n")
 
-    generate_content(client, messages, verbose)
+    generate_content(client, messages, verbose, system_prompt)
 
-def generate_content(client, messages, verbose):
+def generate_content(client, messages, verbose, system_prompt):
     resp = client.models.generate_content(
-        model="gemini-2.0-flash-001", contents=messages
+        model="gemini-2.0-flash-001", contents=messages, config=types.GenerateContentConfig(system_instruction= system_prompt),
     )
     if resp is not None and resp.usage_metadata is not None and verbose:
         print(f"Responsie: {resp.text}")
